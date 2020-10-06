5da81418b29e657510c642acccd1bc34056633a9
Crazyshooting <66379820+Crazyshooting@users.noreply.github.com>
2020-10-06 10:56:12 +0300

    Create slowmode.js | Install Package: ms

---
+++ src/commands/Moderation/slowmode.js
@@ -0,0 +1,72 @@
+
+const ms = require('ms');
+const Command = require('../../core/Command');
+
+class Slowmode extends Command {
+    constructor(rice) {
+        super(rice, {
+            name: 'slowmode',
+            category: 'Moderation',
+            botPerms: ['manageChannels'],
+            memberPerms: ['manageChannels'],
+            description: 'Add a slowmode to a specific channel'
+        });
+    }
+
+    async run(message, args) {
+        const authorTag = `${message.author.username}#${message.author.discriminator}`;
+
+        const disableArgs = ['off', 'stop', 'disable', 'false'];
+
+        const channelForSlowmode = message.channel.guild.channels.get(message.channelMentions[0]) || message.channel;
+
+        if (disableArgs.includes(args[0])) {
+            if (channelForSlowmode.rateLimitPerUser == 0) {
+                return message.channel.send({ embed: { description: `<:no:762884741069275156>  **${authorTag}** Slowmode isn't enabled in that channel!`, color: 0xff4949 } });
+            }
+            
+            await channelForSlowmode.edit({ rateLimitPerUser: 0 }).then(() => {
+                return message.channel.send({ embed: { description: `<:yes:762884751832252417> **${authorTag}** Disabled slowmode in ${channelForSlowmode.mention}`, color: 0x3cb474 } });
+            }).catch((e) => {
+                return message.channel.send({ embed: { description: `<:no:762884741069275156> Something went wrong... I couldn't disable slowmode in that channel.`, color: 0xff4949 } });
+            })
+        } else {
+
+            if (!args[0]) {
+                return message.channel.send({ embed: { description: `<:no:762884741069275156> **${authorTag}** Specify a valid time!`, color: 0xff4949 } });
+            }
+
+            const time = args[0];
+
+            let timeInSeconds = ms(time) / 1000;
+
+            if (isNaN(timeInSeconds)) {
+                return message.channel.send({ embed: { description: `<:no:762884741069275156> **${authorTag}** Specify a valid time!`, color: 0xff4949 } });
+            }
+
+            if (timeInSeconds > 21600) timeInSeconds = 21600;
+
+            if (timeInSeconds == 0) {
+                if (channelForSlowmode.rateLimitPerUser == 0) {
+                    return message.channel.send({ embed: { description: `<:no:762884741069275156>  **${authorTag}** Slowmode isn't enabled in that channel!`, color: 0xff4949 } });
+                }
+                await channelForSlowmode.edit({ rateLimitPerUser: 0 }).then(() => {
+                    return message.channel.send({ embed: { description: `<:yes:762884751832252417> **${authorTag}** Disabled slowmode in ${channelForSlowmode.mention}`, color: 0x3cb474 } });
+                }).catch((e) => {
+                    return message.channel.send({ embed: { description: `<:no:762884741069275156> Something went wrong... I couldn't disable slowmode in that channel.`, color: 0xff4949 } });
+                })
+            }
+
+            let prettyTime = time;
+            if (ms(prettyTime) > 21600000) prettyTime = 21600000;
+            else prettyTime = ms(time);
+            await channelForSlowmode.edit({ rateLimitPerUser: parseInt(timeInSeconds) }).then(() => {
+                return message.channel.send({ embed: { description: `<:yes:762884751832252417> **${authorTag}** Set slowmode of ${channelForSlowmode.mention} to \`${ms(prettyTime, { long: true })}\``, color: 0x3cb474 } });
+            }).catch((e) => {
+                return message.channel.send({ embed: { description: `<:no:762884741069275156> Something went wrong... I couldn't set slowmode to that channel.`, color: 0xff4949 } });
+            })
+        }
+    }
+}
+
+module.exports = Slowmode;
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,7 +7,7 @@ module.exports = class {
     }
 
     async run(message) {
-        let data = await PREFIX.findOne({guildID: message.channel.guild.id})
+        let data = await PREFIX.findOne({ guildID: message.channel.guild.id })
 
 
         const developers = ['699312838455459911', '423687326405885957', '329220047824486400', '521677874055479296', '373293135704621077', '695520751842885672', '515204641450098704']
@@ -15,17 +15,14 @@ module.exports = class {
         if (message.channel.guild === undefined) return;
         if (message.author.bot) return;
 
-        var prefix
-        
-        if(data){
+        var prefix;
+
+        if (data) {
             prefix = data.prefix;
 
-        }else{
+        } else {
             prefix = 'rice ';
         }
-
-        
-
 
         if (!message.content.toLowerCase().startsWith(prefix)) return;
 
@@ -45,46 +42,45 @@ module.exports = class {
         let memberpermsstring = memberperms.join(', ')
         let member_do_return = false
         memberperms.forEach(perm => {
-            if(!message.member.permission.has(perm)) member_do_return = true;
+            if (!message.member.permission.has(perm)) member_do_return = true;
         })
-        if(member_do_return == true){
+        if (member_do_return == true) {
             return message.channel.send({
                 embed: {
-                    title:'Missing permission(s)',
+                    title: 'Missing permission(s)',
                     description: `Permission(s) needed: ${memberpermsstring}`,
                     color: 0xFFFFFd
                 }
             })
         }
-        
+
         let botperms = command.config.botPerms;
         let botpermsstring = memberperms.join(', ')
         let bot_do_return = false;
         let botmemberperms = message.channel.guild.members.find(x => x.id == this.rice.user.id).permission;
-        if(!botmemberperms.has('embedLinks')){
-            try{
+        if (!botmemberperms.has('embedLinks')) {
+            try {
                 return this.rice.getDMChannel(message.author.id).then(ch => {
                     ch.createMessage(`I do not have embedLinks permission in **${message.channel.guild.name}** Please tell a admin to give me embedLinks or if you are a admin you could give me the permission to embedLinks`)
                 })
-            }catch(err){
+            } catch (err) {
                 return;
             }
-            
+
         }
         botperms.forEach(perm => {
-            if(!botmemberperms.has(perm)) bot_do_return = true;
+            if (!botmemberperms.has(perm)) bot_do_return = true;
         })
-        if(bot_do_return == true){
+        if (bot_do_return == true) {
             return message.channel.send({
                 embed: {
-                    title:'Missing bot permission(s)',
+                    title: 'Missing bot permission(s)',
                     description: `Permission(s) needed: ${botpermsstring}`,
                     color: 0xFFFFFd
                 }
             })
         }
         /*Perm checks*/
-
 
         try {
             command.run(message, args, developers);
