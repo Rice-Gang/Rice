9a3a33fd6b5fd80564d8c29cd0835b2ed5dcd4fb
settlelol <settle@dr.com>
2020-10-06 20:21:08 +0100

    AUot

--- src/commands/Config/autorole.js
+++ src/commands/Config/autorole.js
@@ -15,8 +15,8 @@ class Autorole extends Command {
         if (!['true', 'false', 'on', 'off', 'disable', 'enable'].includes(args[0])) {
             return message.channel.sendError(`You need to choose an option between \`true\`, \`false\` or \`view\``)
         }
-        if (args[0] === 'true' || args[0] === 'on' || args[0] === 'enable') {
-            const role = message.channel.guild.roles.find(x => x.name === args.slice(1).join(' ')) || message.channel.guild.roles.get(args[0]);
+        if (args[1] === 'true' || args[1] === 'on' || args[1] === 'enable') {
+            const role = message.channel.guild.roles.find(x => x.name === args.slice(2).join(' ')) || message.channel.guild.roles.get(args[2]);
 
             if (!role || !args[1]) {
                 return message.channel.sendError('Provide a role name (cAsE sEnSeTiVe) or role ID.')
@@ -27,14 +27,14 @@ class Autorole extends Command {
 
             return message.channel.sendSuccess(`Set the autorole in this guild to <@&${role.id}>`)
 
-        } else if (args[0] === 'false' || args[0] === 'off' || args[0] === 'disable') {
+        } else if (args[1] === 'false' || args[1] === 'off' || args[1] === 'disable') {
 
             data.guild.autorole = 'none';
             await data.guild.save();
 
             return message.channel.sendSuccess(`Disabled autorole in this guild.`)
 
-        } else if (args[0] === 'view') {
+        } else if (args[1] === 'view') {
 
             if (data.guild.autorole === 'none') {
                 return message.channel.sendError('This guild has no autorole set.')
