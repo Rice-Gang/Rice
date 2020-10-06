a47744c1deb886a2f1f8aea2b596012478b30dde
loveisglitchy <72410854+loveisglitchy@users.noreply.github.com>
2020-10-06 11:41:06 -0500

    new roll command

---
+++ src/commands/Fun/roll.js
@@ -0,0 +1,20 @@
+const Command = require('../../core/Command');
+
+class Roll extends Command {
+    constructor(rice) {
+        super(rice, {
+            name: 'roll',
+            category: 'fun',
+            botPerms: [''],
+            memberPerms: [],
+            description: 'roll some dice'
+        });
+    }
+
+    async run(message, args) {
+
+        return message.channel.send('rolled a ' + Math.floor(Math.random() * 12) +5)
+    }
+}
+
+module.exports = Roll;
