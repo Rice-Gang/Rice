6d8ce12cde60e9fa2344a2571c14a4047a7c2402
Drak <65980070+Drakmario@users.noreply.github.com>
2020-10-06 14:24:10 -0700

    PP COMMAND
    Yes I know, I use var. Also I'll add more PPs it's just i want to test this cmd out before I do so.

---
+++ src/commands/Fun/pp.js
@@ -0,0 +1,31 @@
+const Command = require('../../core/Command');
+var penis = ["8D", "8=D", '8==D'];
+
+class pp extends Command {
+    constructor(rice) {
+        super(rice, {
+            name: 'pp',
+            category: 'Fun',
+            botPerms: ['sendMessages'],
+            memberPerms: [],
+            description: 'Shows a user pp size'
+        });
+    }
+
+    
+    async run(message, args) {
+        var randomIndex = Math.floor(Math.random() * penis.length); 
+
+        const member = message.mentions[0];
+        const author = message.author;
+
+        if (!args[0]) {
+            message.channel.send(`<@${message.author.id}> pp size is:`+ penis[randomIndex])
+        }
+        else {
+            message.channel.send(`<@${message.member.id}> pp size is:`+ penis[randomIndex])
+        }
+    }
+}
+
+module.exports = pp;
