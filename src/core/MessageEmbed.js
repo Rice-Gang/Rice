b154b5c5e2e10fcabb89b9a84a9e44d4d1e6926e
Gavin Vu <62964708+Gav-King@users.noreply.github.com>
2020-10-06 13:58:43 -0400

    Update MessageEmbed.js

--- src/core/MessageEmbed.js
+++ src/core/MessageEmbed.js
@@ -3,24 +3,24 @@ const fieldArray = [];
 class MessageEmbed {   
     constructor() {
         return embedObject;
-    }
+    };
     setTitle(text) {
         if (!text) throw new Error('You didn\'t provide any text for the title');
         embedObject.title = text; 
         return;
-    }
+    };
 
     setColor(hex) {
         if (!hex) throw new Error("You didn't provide a color.");
         embedObject.color = hex;
         return;
-    }
+    };
 
     setURL(url) {
         if (!url) throw new Error("You didn't provide a url.");
         embedObject.url = url;
         return;
-    }
+    };
 
     setAuthor(text, url) {
         if (!text) throw new Error('Author can\'t be empty.');
@@ -29,19 +29,19 @@ class MessageEmbed {
             embedObject.author.icon_url = url
         }
         return;
-    }
+    };
     setDescription(content) {
         if (!content) throw new Error(`No Content for description provided.`);
 
         embedObject.description = content;
         return;
-    }
+    };
     setThumbnail(url) {
         if (!url) throw new Error(`No Thumbnail URL Provided.`);
 
         embedObject.thumbnail.url = url;
         return;
-    }
+    };
     addField(title, content, position) {
         if (!title) throw new Error(`No Name specified for the field.`);
 
@@ -51,13 +51,13 @@ class MessageEmbed {
 
         embedObject.fields = fieldArray
         return;
-    }
+    };
     setImage(url) {
         if (!url) throw new Error(`No URL for embed image specified.`);
         
         embedObject.image.url = url;
         return;
-    }
+    };
     setFooter(content, url) {
         if (!content) throw new Error(`No Text for footer specified.`);
         
@@ -65,10 +65,10 @@ class MessageEmbed {
         
         if (url) embedObject.icon_url = url
         return;
-    }
+    };
     setTimestamp() {
         embedObject.timestamp = new Date()
         return;
-    }
-}
-module.exports = MessageEmbed
+    };
+};
+module.exports = MessageEmbed;
