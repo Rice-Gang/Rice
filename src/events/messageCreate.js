8a34447bb3cd1b2c86d42a5d8ca84154fb379a64
settlelol <settle@dr.com>
2020-10-06 17:24:59 +0100

    sex

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,3 +1,4 @@
+const data = {};
 
 module.exports = class {
     constructor(rice) {
@@ -5,21 +6,15 @@ module.exports = class {
     }
 
     async run(message) {
-        //let data = await serverConfig.findOne({ guildID: message.channel.guild.id })
-
 
         const developers = ['699312838455459911', '423687326405885957', '329220047824486400', '521677874055479296', '373293135704621077', '695520751842885672', '515204641450098704']
+
+        data.guild = await this.rice.guild(message.channel.guild.id);
 
         if (message.channel.guild === undefined) return;
         if (message.author.bot) return;
 
-        let prefix = 'rice ';
-
-        if (message.content == 'prefix') {
-            message.channel.send(this.rice.db.getPrefix(message.channel.guild.id))
-        }
-
-        // get prefix here.
+        const prefix = data.guild.prefix + ' ' ? data.guild.prefix + ' ' : 'rice '
 
         const prefixRegex = new RegExp(`^(<@!?${this.rice.user.id}>)\\s*`);
         if (prefixRegex.test(message.content)) return message.channel.send(`My prefix in here is \`${prefix}\``);
@@ -58,12 +53,15 @@ module.exports = class {
                 if (!message.channel.permissionsOf(message.author.id).has(p)) {
                     permissions.push(p);
                 }
+                if (permissions.length === 0) {
+                    return;
+                }
                 return message.channel.sendError(`Looks like you are missing some permissions for that command! Here they are: ${permissions.map((p) => `\`${p}\``).join(', ')}`)
             });
         }
 
         try {
-            command.run(message, args);
+            command.run(message, args, data);
         } catch (err) {
             console.log(err);
         }
