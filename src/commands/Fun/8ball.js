b07228eab1caf032c588f3a9e68c927c1cc147f0
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-10-06 07:46:24 -0400

    Create 8ball.js

---
+++ src/commands/Fun/8ball.js
@@ -0,0 +1,31 @@
+const Command = require('../../core/Command');
+const fetch = require('node-fetch')
+const got = require('got')
+
+class Lenny extends Command {
+    constructor(rice) {
+        super(rice, {
+            name: '8ball',
+            category: 'Fun',
+            botPerms: ['attachFiles'],
+            memberPerms: [],
+            description: '8ball'
+        });
+    }
+
+    async run(msg, args) {
+        msg.channel.sendTyping()
+        got('https://nekos.life/api/v2/8ball').then(r => {
+            (async () => {
+                let content = JSON.parse(r.body);
+                let res = await fetch(content.url)
+                let img = await res.buffer()
+                msg.channel.send('', {file: img, name: '8ball.png'})
+            })()
+            
+        })
+        
+    }
+}
+
+module.exports = Lenny;
