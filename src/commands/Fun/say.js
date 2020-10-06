0b46c287e8af340a5f41bb53417a26f5223561f4
Drak <65980070+Drakmario@users.noreply.github.com>
2020-10-06 13:11:47 -0700

    Created say cmd
    If no content is provided then the bot will send a blank embed. Message will be sent as a embed to prevent members from mentioning everyone!!!

---
+++ src/commands/Fun/say.js
@@ -0,0 +1,23 @@
+const Command = require('../../core/Command');
+
+class say extends Command {
+    constructor(rice) {
+        super(rice, {
+            name: 'say',
+            category: 'Fun',
+            botPerms: ['sendMessages'],
+            memberPerms: [],
+            description: 'Say a message'
+        });
+    }
+
+    
+    async run(message, args) {
+        const sayMessage = args.join(" ");
+
+        message.channel.send({ embed: {description: `sayMessage`, color: f6f2c5} });
+
+    }
+}
+
+module.exports = say;
