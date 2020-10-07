3aac4afceadcf277073bdbedaf32cf5a7243f6a5
Gavin Vu <62964708+Gav-King@users.noreply.github.com>
2020-10-07 09:00:43 -0400

    Update Guild.js

--- src/helpers/models/Guild.js
+++ src/helpers/models/Guild.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 
 const Guild = new mongoose.Schema({
-
     id: { type: String },
-
     prefix: { type: String, default: 'rice ' },
-
     muteRole: { type: String, default: ''},
-
     logID: { type: String, default: ''},
-
     autorole: { type: String, default: 'none' },
+    warns: { type: Array, default: [] },
 });
 
 module.exports = mongoose.model('Guild', Guild);
