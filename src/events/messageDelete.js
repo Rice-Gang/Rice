6469255df273aa590d7909fa1dad06a0151a92ec
settlelol <settle@dr.com>
2020-10-07 16:23:12 +0100

    idiot :joy_cat:

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -8,15 +8,15 @@ module.exports = class {
             const snipes = this.rice.snipes.get(message.channel.id) || [];
 
             snipes.unshift({
-                tag: message.author.username,
+                tag: message.author.username + '#' + message.author.discriminator,
                 avatar: message.author.avatarURL,
                 content: message.content
             });
     
             snipes.splice(10);
             this.rice.snipes.set(message.channel.id, snipes);
-        }catch (err){
-            //So it does not log an err when a message is deleted that is not in the cache
+        } catch (err) {
+            return;
         }
         
     }
