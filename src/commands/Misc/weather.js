fac22553bfcfa6e810b36e8057f4488998290295
D3monSlayer64 <65085595+D3monSlayer64@users.noreply.github.com>
2020-10-07 07:29:58 -0400

    deleted useless variable

--- src/commands/Misc/weather.js
+++ src/commands/Misc/weather.js
@@ -15,59 +15,61 @@ class weather extends Command {
 
   async run(message, args) {
     const search = args.join(" ");
-    if (!args[0]) return message.channel.send(message.author.mention+', Weather for what location?')
-    
-    const lmao = await weathers.find(
-      { search: search, degreeType: "F" },
-      function (error, result) {
-        if (error) return console.log(error);
+    if (!args[0])
+      return message.channel.send(
+        message.author.mention + ", Weather for what location?"
+      );
+    await weathers.find({ search: search, degreeType: "F" }, function (
+      error,
+      result
+    ) {
+      if (error) return console.log(error);
 
-        if (result === undefined || result.length === 0)
-          return message.channel.send(`Never heard of ${search}.`);
-        const current = result[0].current;
-        const location = result[0].location;
-        const embed = {
-          title: `Weather forecast for ${current.observationpoint}`,
-          color: 0xfffffd,
-          timestamp: new Date(),
-          fields: [
-            {
-              name: `Temperature:`,
-              value: `${current.temperature}°F`,
-              inline: true,
-            },
-            {
-              name: `Timezone:`,
-              value: `UTC${location.timezone}`,
-              inline: true,
-            },
-            {
-              name: `Wind:`,
-              value: `${current.winddisplay}`,
-              inline: true,
-            },
+      if (result === undefined || result.length === 0)
+        return message.channel.send(`Never heard of ${search}.`);
+      const current = result[0].current;
+      const location = result[0].location;
+      const embed = {
+        title: `Weather forecast for ${current.observationpoint}`,
+        color: 0xfffffd,
+        timestamp: new Date(),
+        fields: [
+          {
+            name: `Temperature:`,
+            value: `${current.temperature}°F`,
+            inline: true,
+          },
+          {
+            name: `Timezone:`,
+            value: `UTC${location.timezone}`,
+            inline: true,
+          },
+          {
+            name: `Wind:`,
+            value: `${current.winddisplay}`,
+            inline: true,
+          },
 
-            {
-              name: `Day:`,
-              value: `${current.shortday}`,
-              inline: true,
-            },
-            {
-              name: `Feels like:`,
-              value: `${current.feelslike}°F`,
-              inline: true,
-            },
-            {
-                name: `Humidity:`,
-                value: `${current.humidity}%`,
-                inline: true,
-              },
-          ],
-        };
+          {
+            name: `Day:`,
+            value: `${current.shortday}`,
+            inline: true,
+          },
+          {
+            name: `Feels like:`,
+            value: `${current.feelslike}°F`,
+            inline: true,
+          },
+          {
+            name: `Humidity:`,
+            value: `${current.humidity}%`,
+            inline: true,
+          },
+        ],
+      };
 
-        message.channel.send({ embed: embed });
-      }
-    );
+      message.channel.send({ embed: embed });
+    });
   }
 }
 
