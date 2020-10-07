3e2ba16c9c0976557355b92916ec2235235f43d3
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-10-07 06:41:53 -0400

    no lower, new bot stats,

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -4,16 +4,20 @@ module.exports = class {
     }
 
     async run(message) {
+        try{
+            const snipes = this.rice.snipes.get(message.channel.id) || [];
 
-        const snipes = this.rice.snipes.get(message.channel.id) || [];
-
-        snipes.unshift({
-            tag: message.author.username + '#' + message.author.discriminator,
-            avatar: message.author.avatarURL,
-            content: message.content
-        });
-
-        snipes.splice(10);
-        this.rice.snipes.set(message.channel.id, snipes);
+            snipes.unshift({
+                tag: message.author.username,
+                avatar: message.author.avatarURL,
+                content: message.content
+            });
+    
+            snipes.splice(10);
+            this.rice.snipes.set(message.channel.id, snipes);
+        }catch (err){
+            //So it no log del msg tht is no in cache :0
+        }
+        
     }
 }
