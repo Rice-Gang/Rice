2c283a603ec7da8e452113462527e57735d3f950
loveisglitchy <uncleneedham@gmail.com>
2020-10-08 09:09:56 -0500

    fixed infractions

--- src/commands/Moderation/infractions.js
+++ src/commands/Moderation/infractions.js
@@ -64,65 +64,66 @@ class Infractions extends Command {
                 });
             }
 
-        const member = await message.channel.guild.members.get(user.id);
+            const member = await message.channel.guild.members.get(user.id);
 
-        /**
-         * TODO: ADD DYNAMIC PAGINATE
-         */
-        number = 5 * parseInt(args[1]);
-        let page;
+            /**
+             * TODO: ADD DYNAMIC PAGINATE
+             */
+            number = 5 * parseInt(args[1]);
+            let page;
 
-        const data = await this.rice.member(user.id);
+            const data = await this.rice.member(user.id);
 
-        if (data.warns.length <= 5) page = 1;
-        else if (data.warns.length <= 10) page = 2;
-        else if (data.warns.length <= 15) page = 3;
-        else if (data.warns.length <= 20) page = 4;
-        else if (data.warns.length <= 25) page = 5;
-        else if (data.warns.length <= 30) page = 6;
-        else if (data.warns.length <= 35) page = 7;
-        else if (data.warns.length <= 40) page = 8;
-        else if (data.warns.length <= 45) page = 9;
-        else if (data.warns.length <= 50) page = 10;
-        else if (data.warns.length <= 55) page = 11;
-        else if (data.warns.length <= 60) page = 12;
+            if (data.warns.length <= 5) page = 1;
+            else if (data.warns.length <= 10) page = 2;
+            else if (data.warns.length <= 15) page = 3;
+            else if (data.warns.length <= 20) page = 4;
+            else if (data.warns.length <= 25) page = 5;
+            else if (data.warns.length <= 30) page = 6;
+            else if (data.warns.length <= 35) page = 7;
+            else if (data.warns.length <= 40) page = 8;
+            else if (data.warns.length <= 45) page = 9;
+            else if (data.warns.length <= 50) page = 10;
+            else if (data.warns.length <= 55) page = 11;
+            else if (data.warns.length <= 60) page = 12;
 
-        if (!args[1]) {
-            number = 5;
-        }
-
-        const warns = data.warns.slice(number - 5, number);
-
-        const embed = {
-            author: {
-                name: member.user.username + '#' + member.user.discriminator,
-                icon_url: member.user.avatarURL
-            },
-            color: null,
-            description: null,
-            fields: [],
-            footer: {
-                text: null
+            if (!args[1]) {
+                number = 5;
             }
-        };
 
-        if (warns.length < 1) {
-            embed.color = 0xff0000;
-            embed.description = 'This user has no infractions.';
-            embed.footer.text = ' ';
-        } else {
-            warns.forEach((s) => {
-                embed.color = 0xd6d3c1;
-                embed.fields.push({ name: this.firstLUC(s.typeOf) + ' | #' + s.case, value: 'Moderator', value: `<@!${s.moderator}> - ${s.reason}`, inline: false });
-                embed.footer.text = `Page ${args[1] || 1} / ${page}`
-            });
+            const warns = data.warns.slice(number - 5, number);
+
+            const embed = {
+                author: {
+                    name: member.user.username + '#' + member.user.discriminator,
+                    icon_url: member.user.avatarURL
+                },
+                color: null,
+                description: null,
+                fields: [],
+                footer: {
+                    text: null
+                }
+            };
+
+            if (warns.length < 1) {
+                embed.color = 0xff0000;
+                embed.description = 'This user has no infractions.';
+                embed.footer.text = ' ';
+            } else {
+                warns.forEach((s) => {
+                    embed.color = 0xd6d3c1;
+                    embed.fields.push({ name: this.firstLUC(s.typeOf) + ' | #' + s.case, value: 'Moderator', value: `<@!${s.moderator}> - ${s.reason}`, inline: false });
+                    embed.footer.text = `Page ${args[1] || 1} / ${page}`
+                });
+            }
+
+            return message.channel.createMessage({ embed });
         }
 
-        return message.channel.createMessage({ embed });
-    }
-
-    firstLUC(str) {
+        firstLUC(str)
         return str.charAt(0).toUpperCase() + str.slice(1);
+
     }
 }
 
