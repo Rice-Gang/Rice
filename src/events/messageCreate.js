3205878f8c651c7aaa2664d95043d6635fb453e0
settlelol <settle@dr.com>
2020-10-08 20:04:16 +0100

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -25,7 +25,7 @@ module.exports = class {
         const msg = message.cleanContent.toLowerCase.split(' ');
         const cmd = msg[1];
         const args = msg.slice(2);
-        const cleanArgs = message.cleanContent.split(' ').slice(2);
+        const cleanArgs = message.cleanContent.split(' ');
 
         const command = this.rice.commands.get(cmd) || this.rice.commands.get(this.rice.aliases.get(cmd));
 
