6feae297bd37dfcf5b63ed2a47164c6991a58780
loveisglitchy <uncleneedham@gmail.com>
2020-10-09 05:16:41 -0500

    .

--- src/commands/Misc/embedtest.js
+++ src/commands/Misc/embedtest.js
@@ -15,20 +15,20 @@ class EmbedTest extends Command {
         const Embed = new MessageEmbed()
             .setTitle('Some title')
             .setColor(0xFFFFFd)
-            // .setURL('https://discord.js.org/')
-            // .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-            // .setDescription('Some description here')
-            // .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-            .addFields( ['hi',  'hi'])
-                // { name: 'Regular field title', value: 'Some value here', inline: false},
-                // { name: '\u200B', value: '\u200B' },
-                // { name: 'Inline field title', value: 'Some value here', inline: true},
-                // { name: 'Inline field title', value: 'Some value here', inline: true},
-            
-            //.addField('Inline field title', 'Some value here', true)
-            // .setImage('https://i.imgur.com/wSTFkRM.png')
-            // .setTimestamp()
-            // .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+            .setURL('https://discord.js.org/')
+            .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .addFields([
+                { name: 'Regular field title', value: 'Some value here', inline: false},
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true},
+                { name: 'Inline field title', value: 'Some value here', inline: true},
+            ])
+            .addField('Inline field title', 'Some value here', true)
+            .setImage('https://i.imgur.com/wSTFkRM.png')
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
             console.log(Embed)
         message.channel.send({ embed: Embed });
     }
