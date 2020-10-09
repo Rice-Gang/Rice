34bf6cb10cd40bdb90c1118990e7ecadf82cafc0
Crazyshooting <66379820+Crazyshooting@users.noreply.github.com>
2020-10-09 13:30:05 +0300

    Some updates

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -15,26 +15,30 @@ class Ban extends Command {
 
     async run(message, args) {
         const authorTag = `${message.author.username}#${message.author.discriminator}`;
-        const userMentions = message.mentions;
-        if (!userMentions[0] || !message.channel.guild.members.get(userMentions[0].id)) {
+        const userMentions = message.mentions[0] || this.rice.users.get(args[0]) || this.rice.users.find(x => x.username == args[0]);
+        if (!userMentions || !message.channel.guild.members.get(userMentions.id)) {
             return message.channel.send({ embed: { description: `<:no:762884741069275156> **${authorTag}** Provide a valid member to ban.`, color: 0xff4949 } });
         }
 
-        if (userMentions[0].id == message.author.id) {
+        if (userMentions.id == message.author.id) {
             return message.channel.send({ embed: { description: `<:no:762884741069275156> **${authorTag}** You can't ban yourself.`, color: 0xff4949 } });
         }
-        const memberToBan = message.channel.guild.members.get(userMentions[0].id);
+        const memberToBan = message.channel.guild.members.get(userMentions.id);
 
         const highestAuthorRole = Math.max.apply(Math, message.member.roles.map((x) => {
             return x.position;
         }));
-        const highestTargetRole = Math.max.apply(Math, memberToBan.roles.map((x) => {
-            return x.position;
+        const highestTargetRole = Math.max.apply(Math, memberToBan.roles.map((e) => {
+            return e.position;
         }));
-        if (highestAuthorRole <= highestTargetRole && message.author.id != message.channel.guild.ownerID) {
+
+        if (parseInt(highestAuthorRole) <= parseInt(highestTargetRole) && message.author.id != message.channel.guild.ownerID) {
             return message.channel.send({ embed: { description: `<:no:762884741069275156> **${authorTag}** You can't ban that member!`, color: 0xff4949 } });
         }
         //<:yes:762884751832252417>
+        if (memberToBan.user.id == message.channel.guild.ownerID) {
+            return message.channel.send({ embed: { description: `<:no:762884741069275156> **${authorTag}** You can't ban the server owner!`, color: 0xff4949 } });
+        }
         const reason = args.slice(1).join(' ');
         const targetTag = `${memberToBan.user.username}#${memberToBan.user.discriminator}`;
         await memberToBan.ban().then(() => {
