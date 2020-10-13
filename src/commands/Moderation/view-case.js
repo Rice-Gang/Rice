844726d8d3c6d67d810f31d7602bbc39d97d4e12
settlelol <settle@dr.com>
2020-10-13 15:44:10 +0100

    Update view-case.js

--- src/commands/Moderation/view-case.js
+++ src/commands/Moderation/view-case.js
@@ -1,66 +1,83 @@
-const Command = require("../../core/Command");
-const MessageEmbed = require('../../core/MessageEmbed')
+const Command = require('../../core/Command');
+
+const Embed = require('../../core/MessageEmbed');
 
 class Viewcase extends Command {
     constructor(client) {
         super(client, {
-            name: "view-case",
-            category: "Moderation",
+            name: 'view-case',
+            category: 'Moderation',
             aliases: ['vc'],
-            memberPerms: ["manageMessages"],
+            memberPerms: ['manageMessages'],
             botPerms: []
         });
     }
 
     async run(message, args, data) {
-        const tag = `${message.author.username}#${message.author.discriminator}`;
-        if (!args[0]) return message.channel.send(`**${tag}**, Provide a valid case!`);
-        else if (args[0] === '--all') {
-            const guildFound = await this.rice.guild(message.guildID);
-            if (guildFound.warns.length < 1)
-                return message.channel.sendError('This guild has no warns.')
-            }
-            let page;
-            let amount = 5 * parseInt(args[1] || 1);
-            if (guildFound.warns.length <= 5) page = 1;
-            else if (guildFound.warns.length <= 10) page = 2;
-            else if (guildFound.warns.length <= 15) page = 3;
-            else if (guildFound.warns.length <= 20) page = 4;
-            let array = guildFound.warns.map(warn => `\`${warn.Case}\` - ${this.rice.users.get(warn.User).username}#${this.rice.users.get(warn.User).discriminator}\n\n**Warned By**: ${this.rice.users.get(warn.Mod).username}#${this.rice.users.get(warn.Mod).discriminator}\n**Reason**: ${warn.Reason}`);
-            array = array.slice(amount - 5, amount);
-            const allWarns = new MessageEmbed()
-            .setTitle(`Warns`)
-            .setDescription(`${array.join('\n\n')}`)
-            .setColor(0xFFFFFd)
-            .setFooter(`Page ${args[1] || 1} of ${page}`);
-            message.channel.send({ embed: allWarns });
+
+        if (!args[0]) {
+            return message.channel.sendError('Provide a valid case number.');
         }
 
-        const caseFound = await this.rice.resolveCase(message.guildID, args[0]);
+        if (message.content.includes('--all')) {
+            if (data.guild.warns.length < 1) {
+                return message.channel.sendError('This guild has no warns.')
+            }
 
-        if (caseFound == null) {
-            return message.channel.send(`**${tag}**, That case doesn't exist`);
+            let page;
+            let amount = 5 * parseInt(args[1] || 1);
+
+            if (data.guild.warns.length <= 5) page = 1;
+            if (data.guild.warns.length <= 10) page = 2;
+            if (data.guild.warns.length <= 15) page = 3;
+            if (data.guild.warns.length <= 20) page = 4;
+            if (data.guild.warns.length <= 25) page = 5;
+            if (data.guild.warns.length <= 30) page = 6;
+
+            let array = data.guild.warns.map(x => `\`${x.Case}\` - ${this.rice.users.get(x.User).username}#${this.rice.users.get(x.User).discriminator}\n\n**Warned by**: ${this.rice.users.get(x.Mod).username}#${this.rice.users.get(x.Mod).discriminator}\n**Reason**: ${x.Reason}`);
+            array = array.slice(amount - 5, amount);
+
+            const embed = {
+                title: `${message.channel.guild.name} Warns`,
+                description: array.join('\n\n'),
+                color: 0xFFFFFd,
+                footer: {
+                    text: `Page ${args[1] || 1} / ${page}`
+                }
+            };
+
+            message.channel.send({ embed });
+        }
+
+        const caseFound = await this.rice.resolveCase(message.channel.guild.id, args[0]);
+
+        if (!caseFound) {
+            return message.channel.sendError('That case doesn\'t exist.')
         } else {
-            let warnedBy;
-            if (message.channel.guild.members.get(caseFound.Mod) || this.rice.users.get(caseFound.Mod)) warnedBy = `${this.rice.users.get(caseFound.Mod).username}#${this.rice.users.get(caseFound.Mod).discriminator}`;
-            else warnedBy = `Moderator left, ID: ${caseFound.Mod}`;
-            
+            let mod;
+            if (message.channel.guild.members.get(caseFound.Mod) || this.rice.users.get(caseFound.Mod)) {
+                mod = `${this.rice.users.get(caseFound.Mod).username}#${this.rice.users.get(caseFound.Mod).discriminator}`;
+            } else mod = `Moderator left, ID: ${caseFound.User}`;
+
             let target;
-            if (message.channel.guild.members.get(caseFound.User) || this.rice.users.get(caseFound.User)) target = `${this.rice.users.get(caseFound.User).username}#${this.rice.users.get(caseFound.User).discriminator}`;
-            else target = `Target left, ID: ${caseFound.User}`;
+            if (message.channel.guild.members.get(caseFound.User) || this.rice.users.get(caseFound.User)) {
+                target = `${this.rice.users.get(caseFound.User).username}#${this.rice.users.get(caseFound.User).discriminator}`;
+            }
+
             const embed = {
                 color: 0xFFFFFd,
                 author: {
-                    name: warnedBy,
-                    icon_url: this.rice.users.get(caseFound.Mod).dynamicAvatarURL('png') || undefined
+                    name: mod,
+                    icon_url: this.rice.users.get(caseFound.Mod).dynamicAvatarURL('png')
                 },
-                description: `**Warned By:** ${warnedBy}\n**Target:** ${target}\n**Reason:** ${caseFound.Reason}`,
+                description: `**Warned By**: ${mod}\n**Target**: ${target}\n**Reason**: ${caseFound.Reason}`,
                 timestamp: caseFound.Date,
                 footer: {
                     text: 'Add --all at the end to view all warns in this server.'
                 }
             };
-            message.channel.send({ embed: embed }); 
+
+            message.channel.send({ embed });
         }
     }
 }
